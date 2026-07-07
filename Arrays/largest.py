afb1226c09d9aee3b618a5748b5f130eba06ab8c
rshmthomas-cyber <rshm.thomas@gmail.com>
2026-07-07 17:37:43 +0530

    Added notes

--- Arrays/largest.py
+++ Arrays/largest.py
@@ -9,3 +9,8 @@ def find_largest(arr):
     return largest
 answer = find_largest([3, 5, 2, 8, 1])
 print(answer)
+
+#len(array) is used to find the length of an array. It returns the number of elements in the array. For example, len([3, 5, 2, 8, 1]) would return 5, since there are 5 elements in the array.
+#every element is compared with current largest element.
+#Follows O[n] time complexity, where n is the number of elements in the array.
+#O(1)space complexity as we are using only a single variable to store the largest element.
