061fdbfb8312f2cc1130a4aa718d5293e30af07a
rshmthomas-cyber <rshm.thomas@gmail.com>
2026-07-23 13:46:52 +0530

    Removing Duplicates from sorted array

---
+++ Arrays/remove_duplicates.py
@@ -0,0 +1,30 @@
+"""
+Problem: To remove duplicates from sorted array.
+
+Solution: Use two pointer system
+
+Time complexity: O(n)
+
+Space complexity: O(1)
+"""
+def remove_duplicates(arr):
+    if not arr:
+        return 0
+    i = 0
+    for j in range(1, len(arr)):
+        if arr[i] != arr[j]:
+            arr[i+1] = arr[j]
+            i+=1
+        else:
+            continue
+    return i+1 
+
+
+#test cases
+arr = [1,1,2,3,4,4,5]
+length = remove_duplicates(arr)
+print(f"Length: {length}")
+print(f"Unique: {arr[:length]}")
+print(f"Whole array: {arr}")
+
+  # Output: Length: 5, Unique: arr[:length] = [1, 2, 3, 4, 5]
